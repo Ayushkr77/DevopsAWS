29bf64cb0d09a11d09422ff1fa97b2b115d8a97b
Ayush Kumar <ayushkumar5990@gmail.com>
2025-12-10 22:15:31 +0000

    Auto trigger commit 4

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -16,7 +16,7 @@ def _response(status_code, body_dict):
 
 
 def lambda_handler(event, context):
-    # Log full event for debugging just for rezipping. Github push fir auto trigger 3
+    # Log full event for debugging just for rezipping. Github push fir auto trigger 4
     print("Event:", json.dumps(event))
 
     # Support REST API (httpMethod) & HTTP API (requestContext.http.method)
